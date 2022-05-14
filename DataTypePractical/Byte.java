ff9bb7044caa2cbe974772ef5dd41e0e499db2ff
Adi00000 <86349563+Adi00000@users.noreply.github.com>
2022-05-14 18:15:18 +0530

    Add files via upload

--- DataTypePractical/Byte.java
+++ DataTypePractical/Byte.java
@@ -2,9 +2,9 @@ public class Byte {
 
 	public static void main(String[] args) {
 		
-		byte myByte1,myByte2;   
-		myByte1 = 127;
-		myByte2 = -48;
+		byte B1, B2;   
+		B1 = 127;
+		B2 = -48;
 		System.out.println("Byte 1: " +myByte1);
 		System.out.println("Byte 2: " +myByte2);
 
