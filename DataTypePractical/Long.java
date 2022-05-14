092ea357719fc214632c562868cf9aaf41bcd5f9
Adi00000 <86349563+Adi00000@users.noreply.github.com>
2022-05-14 18:22:52 +0530

    Add files via upload

--- DataTypePractical/Long.java
+++ DataTypePractical/Long.java
@@ -2,10 +2,10 @@ public class Long {
 	
 	public static void main(String args[]) {
 	
-	long myLong1, myLong2, result;
-	myLong1 = 100000000L;
-	myLong2 = 200L;
-	result = myLong1 * myLong2;
+	
+	long myLong1 = 69548956L;
+	long myLong2 =  876L;
+	long result = myLong1 * myLong2;
 	System.out.println("Number 1: " +myLong1);
 	System.out.println("Number 2: " +myLong2);
 	System.out.println("Number 1 * Number 2: " +result);
