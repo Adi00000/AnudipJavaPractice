e015b56b56247d0870cc5697113db57592d04165
Adi00000 <86349563+Adi00000@users.noreply.github.com>
2022-05-14 18:16:07 +0530

    Add files via upload

--- DataTypePractical/Char.java
+++ DataTypePractical/Char.java
@@ -1,13 +1,10 @@
-public class CharVar {
+public class Char {
 
 	public static void main(String[] args) {
 		
 		
-		
-		char marks, grade; 
-		
-		marks = '8';
-		grade = 'B';
+		char marks = '8';
+		char grade = 'B';
 		
 		System.out.println("Marks:" +marks);
 		System.out.println("Grade: "+grade);
